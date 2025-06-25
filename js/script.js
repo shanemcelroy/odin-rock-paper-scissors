6c7d113698f9cfcf9a7daf7457f28931709895e5
Shane McElroy <mcelroysh@gmail.com>
2025-06-24 22:08:00 -0700

    Write the logic to play a single round
    Declared a function that takes the human and computer choice as
    arguments, plays a single round, increments the round winner's score,
    and logs a winner announcement to the console.

--- js/script.js
+++ js/script.js
@@ -17,3 +17,44 @@ function getHumanChoice() {
   let choice = prompt("Rock, paper, or scissors?: ");
   return choice.toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+  // Checks for a tie
+  if (humanChoice === computerChoice) {
+    console.log(`Tie! You both chose ${humanChoice}.`);
+    return;
+  }
+
+  // Human rock functionality
+  if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You win! Rock beats Scissors!");
+    humanScore++;
+    return;
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose! Paper beats Rock!");
+    computerScore++;
+    return;
+  }
+
+  // Human paper functionality
+  if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You win! Paper beats rock!");
+    humanScore++;
+    return;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("You lose! Scissors beats Paper!");
+    computerScore++;
+    return;
+  }
+
+  // Human scissors functionality
+  if (humanChoice === "scissors" && computerChoice == "paper") {
+    console.log("You win! Scissors beats Paper!");
+    humanScore++;
+    return;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("You lose! Rock beats Scissors!");
+    computerScore++;
+    return;
+  }
+}
