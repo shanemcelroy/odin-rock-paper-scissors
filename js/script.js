538e91d0e0f75a788970020e32a4783ecf3ff164
Shane McElroy <mcelroysh@gmail.com>
2025-06-24 22:28:41 -0700

    Write the logic to play 5 rounds
    In order to go from a single-round system to 5 rounds, I wrapped
    the round function inside a play game function, kept track of both
    player's scores, and declared a winner at the end.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
   let randomNum = Math.floor(Math.random() * 3);
-  console.log(randomNum);
+
   if (randomNum === 0) {
     return "rock";
   } else if (randomNum === 1) {
@@ -18,43 +15,60 @@ function getHumanChoice() {
   return choice.toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice) {
-  // Checks for a tie
-  if (humanChoice === computerChoice) {
-    console.log(`Tie! You both chose ${humanChoice}.`);
-    return;
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+  const roundTotal = 5;
+
+  for (let i = 0; i < roundTotal; i++) {
+    playRound(getHumanChoice(), getComputerChoice());
   }
 
-  // Human rock functionality
-  if (humanChoice === "rock" && computerChoice === "scissors") {
-    console.log("You win! Rock beats Scissors!");
-    humanScore++;
-    return;
-  } else if (humanChoice === "rock" && computerChoice === "paper") {
-    console.log("You lose! Paper beats Rock!");
-    computerScore++;
-    return;
-  }
+  console.log(declareWinner(humanScore, computerScore));
 
-  // Human paper functionality
-  if (humanChoice === "paper" && computerChoice === "rock") {
-    console.log("You win! Paper beats rock!");
-    humanScore++;
-    return;
-  } else if (humanChoice === "paper" && computerChoice === "scissors") {
-    console.log("You lose! Scissors beats Paper!");
-    computerScore++;
-    return;
-  }
+  function playRound(humanChoice, computerChoice) {
+    // Checks for a tie
+    if (humanChoice === computerChoice) {
+      console.log(`Tie! You both chose ${humanChoice}.`);
+      return;
+    }
 
-  // Human scissors functionality
-  if (humanChoice === "scissors" && computerChoice == "paper") {
-    console.log("You win! Scissors beats Paper!");
-    humanScore++;
-    return;
-  } else if (humanChoice === "scissors" && computerChoice === "rock") {
-    console.log("You lose! Rock beats Scissors!");
-    computerScore++;
-    return;
+    // Human rock functionality
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+      console.log("You win! Rock beats Scissors!");
+      return humanScore++;
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+      console.log("You lose! Paper beats Rock!");
+      return computerScore++;
+    }
+
+    // Human paper functionality
+    if (humanChoice === "paper" && computerChoice === "rock") {
+      console.log("You win! Paper beats rock!");
+      return humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+      console.log("You lose! Scissors beats Paper!");
+      return computerScore++;
+    }
+
+    // Human scissors functionality
+    if (humanChoice === "scissors" && computerChoice == "paper") {
+      console.log("You win! Scissors beats Paper!");
+      return humanScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+      console.log("You lose! Rock beats Scissors!");
+      return computerScore++;
+    }
+  }
+  function declareWinner(humanScore, computerScore) {
+    if (humanScore > computerScore) {
+      return `You win with a score of ${humanScore} to ${computerScore}`;
+    } else if (humanScore < computerScore) {
+      return `You lose with a score of ${humanScore} to ${computerScore}`;
+    } else {
+      return "You both tied!";
+    }
   }
 }
+
+playGame();
