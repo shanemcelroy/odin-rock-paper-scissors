386ff5d29dcf3a8cea5707ef35da5f846964e180
Shane McElroy <mcelroysh@gmail.com>
2025-06-30 22:52:55 -0700

    Add a DIV for displaying round results

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 const choices = document.querySelector(".choices");
+const content = document.querySelector(".content");
 const humanScoreText = document.querySelector(".human-score");
 const computerScoreText = document.querySelector(".computer-score");
 
+const msgDiv = document.createElement("div");
+const message = document.createElement("p");
+
 let humanScore = 0;
 let computerScore = 0;
-let roundCount = 0;
 let gameOver = false;
 
 choices.addEventListener("click", (e) => {
   const humanChoice = e.target.textContent.toLowerCase();
-  // console.log(humanChoice, computerChoice());
   if (!gameOver) {
     playRound(humanChoice, computerChoice());
     isGameOver();
@@ -29,45 +31,28 @@ let computerChoice = function () {
 };
 
 function playRound(humanChoice, computerChoice) {
-  // Checks for a tie
   if (humanChoice === computerChoice) {
-    return console.log(`Tie! You both chose ${humanChoice}.`);
-  }
-
-  // Rock
-  if (humanChoice === "rock" && computerChoice === "scissors") {
-    console.log("You win! Rock beats Scissors!");
+    message.textContent = `Tie! You both chose ${humanChoice}.`;
+    message.setAttribute("style", "text-align: center;");
+    msgDiv.appendChild(message);
+    content.insertBefore(msgDiv, choices);
+    return;
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice == "paper")
+  ) {
+    message.textContent = `You win! ${humanChoice} beats ${computerChoice}!`;
+    message.setAttribute("style", "text-align: center;");
+    msgDiv.appendChild(message);
+    content.insertBefore(msgDiv, choices);
     humanScore++;
     humanScoreText.textContent = humanScore;
-    return;
-  } else if (humanChoice === "rock" && computerChoice === "paper") {
-    console.log("You lose! Paper beats Rock!");
-    computerScore++;
-    computerScoreText.textContent = computerScore;
-    return;
-  }
-
-  // Paper
-  if (humanChoice === "paper" && computerChoice === "rock") {
-    console.log("You win! Paper beats rock!");
-    humanScore++;
-    humanScoreText.textContent = humanScore;
-    return;
-  } else if (humanChoice === "paper" && computerChoice === "scissors") {
-    console.log("You lose! Scissors beats Paper!");
-    computerScore++;
-    computerScoreText.textContent = computerScore;
-    return;
-  }
-
-  // Scissors
-  if (humanChoice === "scissors" && computerChoice == "paper") {
-    console.log("You win! Scissors beats Paper!");
-    humanScore++;
-    humanScoreText.textContent = humanScore;
-    return;
-  } else if (humanChoice === "scissors" && computerChoice === "rock") {
-    console.log("You lose! Rock beats Scissors!");
+  } else {
+    message.textContent = `You lose! ${computerChoice} beats ${humanChoice}!`;
+    message.setAttribute("style", "text-align: center;");
+    msgDiv.appendChild(message);
+    content.insertBefore(msgDiv, choices);
     computerScore++;
     computerScoreText.textContent = computerScore;
     return;
@@ -78,13 +63,19 @@ function isGameOver() {
   if (humanScore === 5) {
     humanScoreText.setAttribute("style", "color: green");
     computerScoreText.setAttribute("style", "color: red");
-    console.log(`You win with a score of ${humanScore} to ${computerScore}`);
+    message.textContent = `You win with a score of ${humanScore} to ${computerScore}.`;
+    message.setAttribute("style", "text-align: center;");
+    msgDiv.appendChild(message);
+    content.insertBefore(msgDiv, choices);
     gameOver = true;
     return;
   } else if (computerScore === 5) {
     computerScoreText.setAttribute("style", "color: green");
     humanScoreText.setAttribute("style", "color: red");
-    console.log(`You lose with a score of ${humanScore} to ${computerScore}`);
+    message.textContent = `You lose with a score of ${humanScore} to ${computerScore}.`;
+    message.setAttribute("style", "text-align: center;");
+    msgDiv.appendChild(message);
+    content.insertBefore(msgDiv, choices);
     gameOver = true;
     return;
   } else {
